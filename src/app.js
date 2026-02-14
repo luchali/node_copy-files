b760cd798bd7201361d0fc1ac1eb256f26655f04
Alina Luchytska <aluchitska@gmail.com>
2026-02-14 23:29:15 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+function main() {
+  const params = process.argv.slice(2);
+  const sourceTerminal = params[0];
+  const destTerminal = params[1];
+
+  if (!sourceTerminal || !destTerminal) {
+    console.error('Please enter source and destination');
+
+    return;
+  }
+
+  if (sourceTerminal === destTerminal) {
+    return;
+  }
+
+  fs.copyFile(sourceTerminal, destTerminal, (err) => {
+    if (err) {
+      console.error('Error copying file:', err.message);
+    }
+  });
+}
+
+main();
