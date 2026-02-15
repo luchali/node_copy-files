d665b0655952bf5474bae8fbbaf9840f8f88b2f3
Alina Luchytska <aluchitska@gmail.com>
2026-02-16 00:44:21 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@
 'use strict';
 
 const fs = require('fs');
+// const path = require('path');
 
 function main() {
   const params = process.argv.slice(2);
   const sourceTerminal = params[0];
   const destTerminal = params[1];
 
-  if (!sourceTerminal || !destTerminal) {
+  if (params.length !== 2 || !sourceTerminal || !destTerminal) {
     console.error('Please enter source and destination');
 
     return;
+  }
+
+  if (sourceTerminal.startsWith('-') || destTerminal.startsWith('-')) {
+    console.error('Flags are not supported');
+    process.exit(1);
   }
 
   if (sourceTerminal === destTerminal) {
