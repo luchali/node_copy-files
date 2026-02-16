4a53b1e501ccf525a72f71d9d3217238f2d9f2b1
Alina Luchytska <aluchitska@gmail.com>
2026-02-16 16:06:27 +0200

    fix solution

--- src/app.js
+++ src/app.js
@@ -1,34 +1,57 @@
-/* eslint-disable no-console */
 'use strict';
 
+/* eslint-disable no-console */
+
 const fs = require('fs');
-// const path = require('path');
 
 function main() {
-  const params = process.argv.slice(2);
-  const sourceTerminal = params[0];
-  const destTerminal = params[1];
+  try {
+    const params = process.argv.slice(2);
+    const sourceTerminal = params[0];
+    const destTerminal = params[1];
 
-  if (params.length !== 2 || !sourceTerminal || !destTerminal) {
-    console.error('Please enter source and destination');
+    if (params.length !== 2 || !sourceTerminal || !destTerminal) {
+      console.error('Please enter source and destination');
+      process.exit(0);
+    } else {
+      if (sourceTerminal.startsWith('-') || destTerminal.startsWith('-')) {
+        console.error('Flags are not supported');
+        process.exit(0);
+      }
 
-    return;
-  }
+      if (sourceTerminal === destTerminal) {
+        return;
+      }
 
-  if (sourceTerminal.startsWith('-') || destTerminal.startsWith('-')) {
-    console.error('Flags are not supported');
-    process.exit(1);
-  }
+      fs.stat(sourceTerminal, (srcErr, srcStats) => {
+        if (srcErr) {
+          console.error('Source file does not exist');
+          process.exit(0);
+        }
 
-  if (sourceTerminal === destTerminal) {
-    return;
-  }
+        if (!srcStats.isFile()) {
+          console.error('Source is not a file');
+          process.exit(0);
+        }
 
-  fs.copyFile(sourceTerminal, destTerminal, (err) => {
-    if (err) {
-      console.error('Error copying file:', err.message);
+        fs.stat(destTerminal, (destErr, destStats) => {
+          if (!destErr && destStats.isDirectory()) {
+            console.error('Destination is a directory');
+            process.exit(0);
+          }
+
+          fs.copyFile(sourceTerminal, destTerminal, (err) => {
+            if (err) {
+              console.error('Error copying file');
+              process.exit(0);
+            }
+          });
+        });
+      });
     }
-  });
+  } catch (err) {
+    console.error('Error copying file:', err.message);
+  }
 }
 
 main();
